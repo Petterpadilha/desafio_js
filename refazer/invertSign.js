5b8f41575f5aa1b752cbc9db59297fcc88ffad1b
petter padilha <petterson.padilha@plantae.agr.br>
2022-06-24 10:47:31 -0400

    invertSign

--- refazer/invertSign.js
+++ refazer/invertSign.js
@@ -4,18 +4,14 @@ do tipo number e retorna um number que é o próprio parâmetro.
 */
 
 function invertSign(val) {
-
-    return val * -1 ? val * -1 : val;
-
+     return val * -1 || val
 }
-
-
 
 // Explicação
 /*
    Função recebe um parâmetro e precisa retornar um número invertido,
    para isso fazemos esse calculo valor * -1
 */
-// console.log(invertSign((1)));
-// console.log(invertSign(-2));
-// console.log(invertSign(0));
+console.log(invertSign(1))
+console.log(invertSign(-2))
+console.log(invertSign(0))
