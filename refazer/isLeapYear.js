8e65c6285dc6d212c1c65d328944cef196549bb9
petter padilha <petterson.padilha@plantae.agr.br>
2022-06-28 07:56:39 -0400

    update

--- refazer/isLeapYear.js
+++ refazer/isLeapYear.js
@@ -11,9 +11,9 @@ Função retorna um parâmetro, que precisa retornar um valor boolean, se o ano 
 para saber se o ano é bisexto new Date(date, 1, 29).getMonth() == 1.
 Para isso vamos checar se o mês de fevereiro for até 29 dias, quando o mês for até 29 é bisexto, senão não é bisexto.
 */
-// console.log(isLeapYear(2144))
-// console.log(isLeapYear(2020))
-// console.log(isLeapYear(1122))
-// console.log(isLeapYear(400))
+console.log(isLeapYear(2144))
+console.log(isLeapYear(2020))
+console.log(isLeapYear(1122))
+console.log(isLeapYear(400))
 
 
